0cdc92e37c4dc2eb1036e854d5a62015f8d98ec6
Landon Waddell <Landon.waddell@gmail.com>
2021-05-03 22:09:13 -0600

    Added Event Listener and prompt.

---
+++ script.js
@@ -0,0 +1,18 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+document.querySelector(`#generate`).addEventListener(`click`, promptMe);
+function promptMe() {
+   var criteria = prompt("Password Criteria");
+   alert (criteria);
+}
