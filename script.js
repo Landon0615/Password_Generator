4cbbc2f5ee8b0429bfeb4fd471896e7465f2d690
Landon Waddell <Landon.waddell@gmail.com>
2021-05-06 20:58:10 -0600

    Added enabled generate password if checkbox is checked. Added boiler plate for random generator. Added password length option.

--- script.js
+++ script.js
@@ -1,40 +1,86 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var numberCheckbox = document.querySelector("input[name=include-numbers]");
+var numbersIsChecked = false;
+var passwordLength = 128;
 //
-var numbers = "123456789" .split(" ")
-var specials = "!%&*+-./<>?~" .split(" ")
-varaLower = "abcdefghijklmnopqrstuvwxyz" .split(" ")
-varaUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" .split(" ")
+var numbers = "123456789".split("");
+var special = "!%&*+-./<>?~".split("");
+var lower = "abcdefghijklmnopqrstuvwxyz".split("");
+var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
 
 //Add event listener to generate button
-document.querySelector("#generate").addEventListener("click",  Passwordcharacters);
-document.querySelector("#include-numbers").addEventListener("click",  writePassword);
-document.querySelector("#special-characters").addEventListener("click",  writePassword);
-document.querySelector("#lower-case").addEventListener("click",  writePassword);
-document.querySelector("#upper-case").addEventListener("click",  writePassword);
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+  
+}
+//slider
+function show_value2(x) {
+  document.getElementById("slider_value2").innerHTML = x;
+  passwordLength = x;
+}
 
+function add_one() {
+  document.f.passwordLengthSlider.value = parseInt(document.f.passwordLengthSlider.value) + 1;
+  show_value2(document.f.passwordLengthSlider.value);
 }
-function show_value2(x)
-{
- document.getElementById("slider_value2").innerHTML=x;
-}
-function add_one()
-{
-  document.f.sld6.value=parseInt(document.f.sld6.value)+1;
-  show_value2(document.f.sld6.value);
-}
-function subtract_one()
-{
-  document.f.sld6.value=parseInt(document.f.sld6.value)-1;
-  show_value2(document.f.sld6.value);
+
+function subtract_one() {
+  document.f.passwordLengthSlider.value = parseInt(document.f.passwordLengthSlider.value) - 1;
+  show_value2(document.f.passwordLengthSlider.value);
 }
 
 //Password Criteria
+function generatePassword() {
+  var password = "";
+  for (let index = 0; index < passwordLength; index++) {
+    password = password + getNextRandom(); 
+  }
+  return password;
+}
 
-//TODO 
+function getNextRandom() {   // add more if statements.
+  var validChoices = [];
+  if (numbersIsChecked) {
+    validChoices = validChoices.concat(numbers);
+  }
+  return validChoices[Math.floor(Math.random() * validChoices.length)];
+}
+
+function generateEnabled() {  // add special, upper,lower.
+  return numbersIsChecked;
+}
+//Check box function
+
+// check box value
+numberCheckbox.addEventListener('change', function() {
+  numbersIsChecked = this.checked;
+  generateBtn.disabled = !generateEnabled();
+});
+
+generateBtn.addEventListener('click', function() {
+  writePassword();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//TODO add event listener to enable and disable generate password for special characters, Lowercase, Uppercase.
+// make snake case camel case
+// Add Special, upper, lower if statments
+// Add generateEnabled special upper and lower.  
